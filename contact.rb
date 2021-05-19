03afc58e126286eb66455e51ca96e11f4b2d1b45
shabnamlouie <shabnam.louie@gmail.com>
2021-05-19 20:26:15 +1000

    contact page change 2 times

--- contact.rb
+++ contact.rb
@@ -13,6 +13,22 @@ class Contact
         @last_name
     end
 
+    def first_last
+        first_name + " " + last_name
+    end
+
+    def last_first
+        last_first = last_name
+        last_first += ", "
+        last_first += first_name
+        if !@middle_name.nil?
+            last_first += " "
+            last_first += middle_name.slice(0,1)
+            last_first += "."
+        end
+        last_first
+    end
+
     def full_name
         full_name = first_name
         if !@middle_name.nil?
@@ -23,14 +39,31 @@ class Contact
     full_name += last_name
     full_name
   end
+
+  def to_s(format = 'full_name')
+    case format
+    when 'full_name'
+        full_name
+    when 'last_first'
+        last_first
+    when 'first'
+        first_name
+    when 'last'
+        last_name
+    else
+        first_last
+    end
+  end
 end
 
 Shabnam = Contact.new
 Shabnam.first_name = "Shabnam"
 Shabnam.last_name = "Louie"
-puts Shabnam.full_name
+puts Shabnam.to_s
+puts Shabnam.to_s('full_name')
+puts Shabnam.to_s('last_first')
 
 Alaw = Contact.new
 Alaw.first_name = "Alaw"
 Alaw.last_name = "Moradi"
-puts Alaw.full_name
+puts Alaw.to_s('full_name')
