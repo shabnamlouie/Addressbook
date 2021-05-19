7315f535fe79ba5529fcdee48a2d003842e95216
shabnamlouie <shabnam.louie@gmail.com>
2021-05-19 22:29:37 +1000

    address page add again

---
+++ address.rb
@@ -0,0 +1,32 @@
+class Address
+    attr_accessor :kind, :street_1, :street_2, :city, :state, :postal_code
+
+    def to_s(format = 'short')
+        address = ''
+        case format
+        when 'long'
+            address += street_1 + "\n"
+            address += street_2 + "\n" if !street_2.nil?
+            address += "#{city}, #{state} #{postal_code}"
+        when 'short'
+            address += "#{kind}: "
+            address += street_1
+            if street_2
+                address += " " + street_2
+        end
+        address += ", #{city}, #{state}, #{postal_code}"
+    end
+    address
+  end
+end
+
+home = Address.new
+home.kind = "Home"
+home.street_1 = "20 Ford street."
+home.city = "Surry hills"
+home.state = "NSW"
+home.postal_code = "2000"
+
+puts home.to_s('short')
+puts "\n"
+puts home.to_s('long')
