f985d8b127f253010505d5b9af83629269836671
shabnamlouie <shabnam.louie@gmail.com>
2021-05-20 15:21:37 +1000

    contact page changed for 6 times and contact.yaml page added

--- address_book.rb
+++ address_book.rb
@@ -1,10 +1,24 @@
 require "./contact"
+require "yaml"
 
 class AddressBook
     attr_reader :contacts
 
     def initialize
         @contacts = []
+        open()
+    end
+
+    def open
+    if File.exist?("contacts.yml")
+    @contacts = YAML.load_file("contacts.yml")
+    end
+    end
+
+    def save
+        File.open("contacts.yaml", "w") do |file|
+            file.write(contacts.to_yaml)
+        end
     end
 
     def run
@@ -12,6 +26,7 @@ class AddressBook
             puts "Address Book"
             puts "a: Add Contact"
             puts "p: Print Address Book"
+            puts "s: Search"
             puts "e: Exit"
             print "Enter your choice: "
             input = gets.chomp.downcase
@@ -20,7 +35,14 @@ class AddressBook
                 add_contact
             when 'p'
                 print_contact_list
+            when 's'
+                print "Search term: "
+                search = gets.chomp
+                find_by_name(search)
+                find_by_phone_number(search)
+                find_by_address(search)
             when 'e'
+                save()
                 break
             end
         end
