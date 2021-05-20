e995773c776e22febe323f5dd0d1d5d35ce74e07
shabnamlouie <shabnam.louie@gmail.com>
2021-05-20 19:53:23 +1000

    some changes

--- address_book.rb
+++ address_book.rb
@@ -154,25 +154,3 @@ end
 
 address_book = AddressBook.new
 address_book.run
-
-# Shabnam =  Contact.new
-# Shabnam.first_name = "Shabnam"
-# Shabnam.last_name = "Louie"
-# Shabnam.add_phone_number("Home", "123-456-78")
-# Shabnam.add_phone_number("Work", "987-654-32")
-# Shabnam.add_address("Home", "20 Ford st.", "", "Surry hills", "NSW", "2000")
-
-# Sara = Contact.new
-# Sara.first_name = "Sara"
-# Sara.last_name = "Pitt"
-# Sara.add_phone_number("Home", "222-222-222")
-# Sara.add_address("Home", "10 two Jay st", "", "Central", "NSW", "2000")
-
-# address_book.contacts.push(Shabnam)
-# address_book.contacts.push(Sara)
-
-# # address_book.print_contact_list
-
-# # address_book.find_by_name("e")
-# # address_book.find_by_phone_number("2")
-# address_book.find_by_address("two")
